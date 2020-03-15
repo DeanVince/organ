7e81d3f37fca70db6ac56c9126dae47fce0d2bb2
wangshaohua <285992493@qq.com>
2020-03-15 22:27:00 +0800

    新闻发布后页面bug

--- src/main/resources/static/js/appjs/community/news/add.js
+++ src/main/resources/static/js/appjs/community/news/add.js
@@ -26,12 +26,13 @@ function save() {
 		success : function(data) {
 			if (data.code == 0) {
 				parent.layer.msg("操作成功");
-				// parent.layer.reLoad();
-                window.opener.location.href=window.opener.location.href;
-                window.opener.location.reload();
+				if(parent.reLoad){
+                    parent.reLoad();
+                }else {
+                    window.location.reload();
+                }
                 var index = parent.layer.getFrameIndex(window.name); // 获取窗口索引
-				parent.layer.close(index);
-
+                parent.layer.close(index);
 			} else {
 				parent.layer.alert(data.msg)
 			}
